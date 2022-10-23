7bf22774b0e1e6077ca4dddb6552bcec949892c0
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-10-23 15:58:37 +0500

    add new codes for step 2

--- src/Step2.jsx
+++ src/Step2.jsx
@@ -1,5 +1,89 @@
 import React from "react";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
+import { MainContainer } from "./components/MainContainer";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { Form } from "./components/Form";
+import { Input } from "./components/Input";
+import { useData } from "./DataContext";
+import Typography from "@material-ui/core/Typography";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers";
+import { PrimaryButton } from "./components/PrimaryButton";
+import parsePhoneNumberFromString from "libphonenumber-js";
+
+const normalizePhoneNumber = (value) => {
+  const phoneNumber = parsePhoneNumberFromString(value);
+};
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Email should have correct format")
+    .required("Email is a required field"),
+});
 
 export const Step2 = () => {
-  return 
+  const { data, setValues } = useData();
+  const history = useHistory();
+
+  const { register, handleSubmit, watch, errors } = useForm({
+    defaultValues: {
+      email: data.email,
+      hasPhone: data.hasPhone,
+      phoneNumber: data.phoneNumber,
+    },
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
+
+  const hasPhone = watch("hasPhone");
+
+  const onSubmit = (data) => {
+    history.push("/step3");
+    setValues(data);
+  };
+
+  return (
+    <MainContainer>
+      <Typography>🦄 Step 2</Typography>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          ref={register}
+          type="email"
+          label="Email"
+          name="email"
+          required
+          error={!!errors.email}
+          helperText={errors?.email?.message}
+        />
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultValues={data.hasPhone}
+              defaultChecked={data.hasPhone}
+              color={"primary"}
+              inputRef={register}
+              name="hasPhone"
+            />
+          }
+          label="Do you have a phone"
+        />
+        {hasPhone && (
+          <Input
+            ref={register}
+            id="phoneNumber"
+            type="tel"
+            label="Phone Number "
+            name="phoneNumber"
+            onChange={(event) => {
+              event.target.value = normalizePhoneNumber(event.target.value);
+            }}
+          />
+        )}
+        <PrimaryButton>Next</PrimaryButton>
+      </Form>
+    </MainContainer>
+  );
 };
